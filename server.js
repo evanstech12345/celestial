ce3ba2383b0b874b3acce57b96fb9aeedb16799f
evanstech12345 <evanshcode@gmail.com>
2021-08-06 19:54:30 -0400

    init the css in the backend

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const path = require('path')
+
+app.set('view-engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
  
 app.listen(3000, console.log('listening on port 3000'))
