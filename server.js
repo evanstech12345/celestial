7d4c66b3e447c9f8bc756eba2604cc41b6618bed
evanstech12345 <evanshcode@gmail.com>
2021-08-06 20:09:20 -0400

    getting the routes working

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ const path = require('path')
 app.set('view-engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
  
+//import routes
+
+const authRoute = require('./routes/auth')
+
+//routes middleware
+app.use('/api/user', authRoute)
+app.use('/api/login', authRoute)
+
 app.listen(3000, console.log('listening on port 3000'))
