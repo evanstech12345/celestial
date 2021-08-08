b868bafe0f7509fdbb9259e1f46d18d23b62b7ea
evanstech12345 <evanshcode@gmail.com>
2021-08-08 02:00:24 -0400

    getting login to work

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
 const express = require('express');
 const app = express()
 const path = require('path')
@@ -9,13 +14,28 @@ const Joi = require('@hapi/joi');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const axios = require('axios')
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
+// const initilizePassport = require('./passport.config')
+// initilizePassport(passport,
+//     email => users.find(user => user.email === email)
+// )
 
 app.set('view engine', 'ejs')
 // app.set('views', 'view')
 app.use(express.static(path.join(__dirname, '/front/public')));
 app.use(express.static(path.join(__dirname, '/front/public/images')));
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: false }))
+// app.use(flash())
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session)
 dotenv.config();
 
 //connect to db
@@ -30,7 +50,8 @@ console.log('db is connected')
 // get routes
 
 // var bodyParser = require('body-parser')
-const {registerValidation, loginValidation} = require('./validation')
+const {registerValidation, loginValidation} = require('./validation');
+// const passport = require('passport');
 
 app.get('/register', (req, res) => {
 
@@ -65,6 +86,7 @@ app.post('/register', async (req, res) => {
     try{
         const savedUser = await user.save()
         res.send(savedUser)
+        res.redirect('/login')
     } catch(err) {
         res.status(400).send(err)
     }
@@ -94,10 +116,7 @@ app.post('/login', async (req, res) => {
     // const token = jwt.sign(({_id: user._id}), process.env.TOKEN_SECRET)
 
     // res.header(('auth-token', token).send(token))
-
-})
-app.get('/login', (req, res) => {
-    res.render('/opt/homebrew/Caskroom/miniforge/base/envs/celesial/Celesial/front/view/login.ejs')
+    console.log('logged in')
 })
 app.get('/', (req, res) => {
     res.render("/opt/homebrew/Caskroom/miniforge/base/envs/celesial/Celesial/front/view/index.ejs")
